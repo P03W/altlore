1384f1923e8527f800d0b87ead3e4c5e82e39d72
P03W <P03W@users.noreply.github.com>
2021-09-16 22:26:22 -0400

    Initial commit

---
+++ src/main/java/mc/altlore/client/Altlore.java
@@ -0,0 +1,90 @@
+package mc.altlore.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.resource.ResourceManager;
+import net.minecraft.resource.ResourceType;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import org.lwjgl.glfw.GLFW;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+
+@Environment(EnvType.CLIENT)
+public class Altlore implements ClientModInitializer {
+    public static KeyBinding keyBinding;
+    public static HashMap<Item, String[]> lore = new HashMap<Item, String[]>();
+    
+    @Override
+    public void onInitializeClient() {
+        keyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+            "key.altlore.show",
+            InputUtil.Type.KEYSYM,
+            GLFW.GLFW_KEY_RIGHT_ALT,
+            "category.altlore.keybinds"
+        ));
+        
+        ResourceManagerHelper.get(ResourceType.CLIENT_RESOURCES).registerReloadListener(
+            new SimpleSynchronousResourceReloadListener() {
+                @Override
+                public void reload(ResourceManager manager) {
+                    lore.clear();
+                    for(Identifier id : manager.findResources("altlore", path -> path.endsWith(".altlore"))) {
+                        try(InputStream stream = manager.getResource(id).getInputStream()) {
+                            final char[] buffer = new char[8192];
+                            final StringBuilder result = new StringBuilder();
+                            try (Reader reader = new InputStreamReader(stream, StandardCharsets.UTF_8)) {
+                                int charsRead;
+                                while ((charsRead = reader.read(buffer, 0, buffer.length)) > 0) {
+                                    result.append(buffer, 0, charsRead);
+                                }
+        
+                                String[] text = result.toString().split("\n");
+                                handleLines(text);
+                            }
+                        } catch(Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+    
+                private void handleLines(String[] text) {
+                    Arrays.stream(text).forEach(s -> {
+                        String[] slice = s.split("=");
+                        Identifier itemID = new Identifier(slice[0]);
+                        Item item = Registry.ITEM.get(itemID);
+                        if (item == Items.AIR) {
+                            System.err.println("Unable to find item " + itemID);
+                        } else {
+                            String loreTextRaw = Arrays.stream(slice)
+                                .skip(1)
+                                .collect(Collectors.joining("="))
+                                .replaceAll("\n", "")
+                                .replaceAll("\r", "");
+                            System.out.println(Arrays.toString(loreTextRaw.split("\\\\n")));
+                            lore.put(item, loreTextRaw.split("\\\\n"));
+                        }
+                    });
+                }
+    
+                @Override
+                public Identifier getFabricId() {
+                    return new Identifier("altlore", "lore");
+                }
+            });
+    }
+}
---
+++ src/main/java/mc/altlore/mixin/OverrideTooltipTextMixin.java
@@ -0,0 +1,68 @@
+package mc.altlore.mixin;
+
+import com.google.common.collect.Lists;
+import mc.altlore.client.Altlore;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Formatting;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Mixin(ItemStack.class)
+public class OverrideTooltipTextMixin {
+    @Inject(method = "getTooltip", at = @At("RETURN"), locals = LocalCapture.CAPTURE_FAILHARD)
+    public void addHasAltLore(PlayerEntity player, TooltipContext context, CallbackInfoReturnable<List<Text>> cir, List<Text> list) {
+        if (!isPressed() && hasLore()) {
+            list.add(
+                new TranslatableText("message.altlore.holdKey_1")
+                    .formatted(Formatting.DARK_GRAY)
+                    .append(Altlore.keyBinding.getBoundKeyLocalizedText())
+                    .append(new TranslatableText("message.altlore.holdKey_2"))
+            );
+        }
+    }
+    
+    @Inject(method = "getTooltip", at = @At("HEAD"), cancellable = true)
+    public void replaceDisplay(PlayerEntity player, TooltipContext context, CallbackInfoReturnable<List<Text>> cir) {
+        if (isPressed() && hasLore()) {
+            List<Text> list = Lists.newArrayList();
+    
+            list.addAll(
+                Arrays.stream(
+                    Altlore.lore.get(((ItemStack)(Object)this).getItem()))
+                    .map(LiteralText::new)
+                    .collect(Collectors.toCollection(ArrayList::new)
+                    )
+            );
+            
+            cir.setReturnValue(list);
+        }
+    }
+    
+    private boolean hasLore() {
+        return Altlore.lore.containsKey(((ItemStack)(Object)this).getItem());
+    }
+    
+    private boolean isPressed() {
+        if (((KeyBindingCurrentAccessor)Altlore.keyBinding).getBoundKey().getCode() == -1) return false;
+        
+        return InputUtil.isKeyPressed(
+            MinecraftClient.getInstance().getWindow().getHandle(),
+            ((KeyBindingCurrentAccessor)Altlore.keyBinding).getBoundKey().getCode()
+        );
+    }
+}
